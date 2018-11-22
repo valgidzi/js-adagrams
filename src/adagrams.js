5b36e16efffd021b83745ccf4ed41e977236f8ee
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-22 00:09:36 -0800

    Create poolOfLetters array from letterFrequencies object in drawLetters()

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,8 +1,42 @@
 const Adagrams = {
   drawLetters() {
     // Implement this method for wave 1
+    const letterFrequencies = {
+      "A": 9,
+      "B": 2,
+      "C": 2,
+      "D": 4,
+      "E": 12,
+      "F": 2,
+      "G": 3,
+      "H": 2,
+      "I": 9,
+      "J": 1,
+      "K": 1,
+      "L": 4,
+      "M": 2,
+      "N": 6,
+      "O": 8,
+      "P": 2,
+      "Q": 1,
+      "R": 6,
+      "S": 4,
+      "T": 6,
+      "U": 4,
+      "V": 2,
+      "W": 2,
+      "X": 1,
+      "Y": 2,
+      "Z": 1,
+    }
 
-    const poolOfLetters = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F", "G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"]
+    const poolOfLetters = [];
+
+    for (const letter in letterFrequencies) {
+      for (let i = 0; i < letterFrequencies[letter]; i += 1) {
+        poolOfLetters.push(letter);
+      }
+    }
 
     for (let i = poolOfLetters.length - 1; i > 0; i -= 1 ) {
       const j = Math.floor(Math.random() * Math.floor(i));
